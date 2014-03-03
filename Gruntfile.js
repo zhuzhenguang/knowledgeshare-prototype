117720b469fb275fa0c359fa7e524b95fa765b2d
zhuzhenguang <zhuzhenguang2008@gmail.com>
2014-03-03 16:59:01 +0800

    jj

--- Gruntfile.js
+++ Gruntfile.js
@@ -40,13 +40,22 @@ module.exports = function (grunt) {
                 },
                 src: ['src/**/*.js']
             }
-        }
+        },
+		connect: {
+			server:{
+				option: {
+					port:80,
+					keepalive:true
+				}
+			}
+		}
     });
 
     grunt.loadNpmTasks('grunt-contrib-clean');
     grunt.loadNpmTasks('grunt-contrib-concat');
     grunt.loadNpmTasks('grunt-contrib-uglify');
     grunt.loadNpmTasks('grunt-contrib-jshint');
+    grunt.loadNpmTasks('grunt-contrib-connect');
 
     grunt.registerTask('default', ['jshint', 'clean', 'concat', 'uglify']);
 };
